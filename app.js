f4e402bcbecdcddb6fea4970c2eebd1562a4ea35
Sreekanth Namagiri <sreekanth.namagiri@gmail.com>
2018-08-23 00:51:33 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+git init - Initialize the local GIT repository
+git add <file> - Add files to repository
+git status - Check status of the working Tree
+git commit - Commit the changes to index
+git push - Push to Remote Repository
+git pull - Pull Latest From Remote Repository
+git clone - Clone Repository into a New Directory
